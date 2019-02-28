ca1fadce1d6c6d0ebab99d5a0d351c7f5ca35094
awen221 <awen221@yahoo.com.tw>
2019-02-28 23:05:27 +0800

    Game_Rhytnm/Game.h 完成雛型

--- Game_Rhytnm/Game.h
+++ Game_Rhytnm/Game.h
@@ -4,30 +4,122 @@
 #include "point_base.h"
 typedef PointBase<double> obj;
 #include "random.h"
+#include "time_ex.h"
 
 //Rhythm
 class Game
 {
 private:
 
-
 protected:
 	virtual bool InputUp() = 0;
 	virtual bool InputDown() = 0;
 	virtual bool InputLeft() = 0;
 	virtual bool InputRight() = 0;
+
+	TimePass tp;
+	int Score;
+
+
+	const int PerfectScore = 1000;
+	const int GoodScore = 200;
+	const int BadScore = -200;
+
+	const int PerfectRange = 500;
+	const int GoodRange = 1000;
+
+	int noteTime;
+	bool noteUsed;
+
+
+	const int noteStartX = 200;
+
+	const int noteStartY = 500;
+	const int noteRunDistanceY = 400;
+	const int noteDestinationY = noteStartY - noteRunDistanceY;
+
+	int noteX, noteY;
+
+	void GameInit()
+	{
+		Score = 0;
+
+		noteTime = 10 * 1000;
+		noteUsed = false;
+		noteX = noteStartX;
+		noteY = noteStartY;
+	}
+	
+	void GameWork()
+	{
+		int MusicPlayTime = tp.GetTotalTimePass();
+
+		int CurrentY = (MusicPlayTime*noteRunDistanceY) / noteTime;
+		noteY = noteStartY - CurrentY;
+
+		if (!noteUsed)
+		{
+			if (InputUp())
+			{
+				//Perfect
+				if (MusicPlayTime > (noteTime - PerfectRange))
+				{
+					if (MusicPlayTime <(noteTime + PerfectRange))
+					{
+						Score += PerfectScore;
+						noteUsed = true;
+					}
+				}
+				//Good
+				if (MusicPlayTime >(noteTime - GoodRange))
+				{
+					if (MusicPlayTime <(noteTime - PerfectRange))
+					{
+						Score += GoodScore;
+						noteUsed = true;
+					}
+				}
+				if (MusicPlayTime >(noteTime + PerfectRange))
+				{
+					if (MusicPlayTime <(noteTime + GoodRange))
+					{
+						Score += GoodScore;
+						noteUsed = true;
+					}
+				}
+
+				//if (MusicPlayTime <(noteTime - GoodRange))
+				//{
+				//	Score += BadScore;
+				//	noteUsed = true;
+				//}
+
+			}
+
+			//Bad
+			if (MusicPlayTime >(noteTime + GoodRange))
+			{
+				Score += BadScore;
+				noteUsed = true;
+			}
+
+		}
+	}
+
 public:
 	Game() {}
 	~Game() {}
 
 	virtual void Init()
 	{
-
+		tp = TimePass();
+		GameInit();
 	}
 
 	virtual void Work()
 	{
-
+		tp.Work();
+		GameWork();
 	}
 };
 
@@ -35,6 +127,8 @@ public:
 #include "GetAsyncKeyStateManger.h"
 #include "time_ex.h"
 #include "TcString.h"
+#include<tchar.h>
+#include<string.h>
 
 class WinGame :public Game
 {
@@ -43,6 +137,8 @@ private:
 	const int StartY = 60;
 	const int spaceWidth = 720;
 	const int spaceHight = 540;
+
+	const int noteSize = 20;
 
 	KeyStateManager keyStateManager;
 	void KeyStateManager_Init()
@@ -55,40 +151,12 @@ private:
 		keyStateManager = KeyStateManager();
 	}
 
-	//class TimerWin : public Timer
-	//{
-	//public:
-	//	TimerWin(unsigned int len) :Timer(len) {};
-
-	//	void Draw(HDC hdc)
-	//	{
-	//		TcString tString = TcString();
-	//		tString = L"Timer : ";
-	//		static bool OnTimerPluse = false;
-	//		if (GetOnTimer())
-	//		{
-	//			OnTimerPluse = !OnTimerPluse;
-	//		}
-
-	//		if (OnTimerPluse)
-	//		{
-	//			tString += L"O";
-	//		}
-	//		else
-	//		{
-	//			tString += L"X";
-	//		}
-
-	//		TextOut(hdc, 0, 20, tString, tString.len);
-	//	}
-	//};
-	//TimerWin gtr = TimerWin(1000 / 10);
 protected:
 
-	bool InputLeft()final override { return keyStateManager.IsDown(VK_LEFT); }
-	bool InputUp()final override { return keyStateManager.IsDown(VK_UP); }
-	bool InputRight()final override { return keyStateManager.IsDown(VK_RIGHT); }
-	bool InputDown()final override { return keyStateManager.IsDown(VK_DOWN); }
+	bool InputLeft()final override { return keyStateManager.IsTriggerDown(VK_LEFT); }
+	bool InputUp()final override { return keyStateManager.IsTriggerDown(VK_UP); }
+	bool InputRight()final override { return keyStateManager.IsTriggerDown(VK_RIGHT); }
+	bool InputDown()final override { return keyStateManager.IsTriggerDown(VK_DOWN); }
 
 public:
 	void Init()final override
@@ -99,8 +167,6 @@ public:
 	}
 	void Work()final override
 	{
-		//if (!gtr.Work())return;
-
 		keyStateManager.Work();
 
 		Game::Work();
@@ -110,12 +176,21 @@ public:
 		Rectangle(hdc, StartX, StartY, StartX + spaceWidth, StartY + spaceHight);
 
 		TcString tString = TcString();
-		tString = L"BodyCurrentCount : ";
-		TextOut(hdc, 300, 0, tString, tString.len);
 
+		tString = L"經過時間 : ";
+		tString += ((float)tp.GetTotalTimePass()) / 1000;
+		tString += L"秒";
+		TextOut(hdc, 300, 0, tString, tString.len-5);
 
+		tString = L"Score : ";
+		tString += Score;
+		TextOut(hdc, 300, 20, tString, tString.len);
 
-		//Ellipse(hdc, foodX, foodY, foodX + BodySize, foodY + BodySize);
+		MoveToEx(hdc, StartX, noteDestinationY, NULL);
+		LineTo(hdc, StartX + spaceWidth, noteDestinationY);
+
+		if(!noteUsed)
+			Ellipse(hdc, noteX- noteSize, noteY- noteSize, noteX + noteSize, noteY + noteSize);
 	}
 };
 
