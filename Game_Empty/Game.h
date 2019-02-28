fe57336af662eb719448b892814f9c8c6e88a5e0
awen221 <awen221@yahoo.com.tw>
2019-02-28 16:46:38 +0800

    Game_Empty修改

--- Game_Empty/Game.h
+++ Game_Empty/Game.h
@@ -5,16 +5,17 @@
 
 class Game
 {
-private:
+
 
 protected:
-
+	virtual bool InputUp() = 0;
+	virtual bool InputDown() = 0;
+	virtual bool InputLeft() = 0;
+	virtual bool InputRight() = 0;
 
 public:
 	Game() {}
 	~Game() {}
-
-	enum { WindowsWidth = 800, WindowsHeight = 600 };
 
 	virtual void Init()
 	{
@@ -25,20 +26,47 @@ public:
 	}
 };
 
+#include "GetAsyncKeyStateManger.h"
+
 class WinGame :public Game
 {
+private:
+	const int StartX = 40;
+	const int StartY = 60;
+	const int spaceWidth = 720;
+	const int spaceHight = 540;
+
+	KeyStateManager keyStateManager;
+	void KeyStateManager_Init()
+	{
+		keyStateManager.AddKeyState(VK_UP);
+		keyStateManager.AddKeyState(VK_DOWN);
+		keyStateManager.AddKeyState(VK_LEFT);
+		keyStateManager.AddKeyState(VK_RIGHT);
+
+		keyStateManager = KeyStateManager();
+	}
+protected:
+	bool InputLeft()final override { return keyStateManager.IsDown(VK_LEFT); }
+	bool InputUp()final override { return keyStateManager.IsDown(VK_UP); }
+	bool InputRight()final override { return keyStateManager.IsDown(VK_RIGHT); }
+	bool InputDown()final override { return keyStateManager.IsDown(VK_DOWN); }
 public:
 	void Init()final override
 	{
+		KeyStateManager_Init();
+
 		Game::Init();
 	}
 	void Work()final override
 	{
+		keyStateManager.Work();
+
 		Game::Work();
 	}
 	void Draw(HDC hdc)
 	{
-
+		Rectangle(hdc, StartX, StartY, StartX + spaceWidth, StartY + spaceHight);
 	}
 };
 
